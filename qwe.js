97c659cad19ba3316e48acfd9a2e558d4b8ef640
hiichaki <hiichaki350@gmail.com>
2015-10-30 23:50:06 +0200

    create

---
+++ qwe.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<script>
+	var a = [3][3];
+    	for(i=0;i<3;++i)
+    		for(j=0;j<3;++j)
+    			a=Math.rand();
+    	console.log(a);
+	</script>
+</body>
+</html>
+    
+
